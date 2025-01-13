cb8ef8c694160415f1a1dd76c73dee4585e34a0d
Neel <cs1230022@iitd.ac.in>
2025-01-13 23:35:56 +0530

    added code for taking input

--- 1.c
+++ 1.c
@@ -2,13 +2,35 @@
 #include <stdlib.h>
 
 int main() {
+    int R;
+    int C;
+    scanf("%d", &R);
+    scanf("%d", &C);
 
-        int R;
-        int C;
-        scanf("%d", &R);
-        scanf("%d", &C);
+    int *arr = (int *)calloc(R * C, sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
 
-        int *arr = (int *)calloc(R * C, sizeof(int));
+    char a = 'n';
 
-        return 0;
+    while (1) {
+        if (a != 'n') {
+            printf("current input is %c\n", a);
+            a = 'n';
+        }
+
+        else {
+            char b;
+            scanf(" %c", &b);
+            if (b != '\0') {
+                a = b;
+            }
+        }
+    }
+
+    free(arr);
+
+    return 0;
 }

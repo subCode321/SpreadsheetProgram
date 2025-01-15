9cb66a68cca3786483e615cda30112a2aa4451ce
NeelMishra007 <168926792+NeelMishra007@users.noreply.github.com>
2025-01-15 10:54:59 +0530

    Created Separate Scroller Function

--- 1.c
+++ 1.c
@@ -1,17 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printer(int currx, int curry, int *arr, int C) {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+int min(int x, int y)
+{
+    if (x > y)
+        return y;
+    else
+        return x;
+}
+
+void printer(int currx, int curry, int *arr, int C, int R)
+{
+    for (int j = 0; j < min(10, R - curry); j++)
+    {
+        for (int i = 0; i < min(10, C - currx); i++)
+        {
             printf("%d ", arr[(currx + i) + C * (curry + j)]);
         }
         printf("\n");
     }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
+void scroller(char *a, int *arr, int *currx, int *curry, int C, int R)
+{
+    int flag = 0;
+
+    if (a[0] == 'w')
+    {
+        if (*curry < 10)
+        {
+            flag = 1;
+        }
+        else
+        {
+            *curry -= 10;
+            printer(*currx, *curry, arr, C, R);
+        }
+    }
+    else if (a[0] == 'd')
+    {
+        if (*currx >= C - 10)
+        {
+            flag = 1;
+        }
+        else
+        {
+            *currx += 10;
+            printer(*currx, *curry, arr, C, R);
+        }
+    }
+    else if (a[0] == 'a')
+    {
+        if (*currx < 10)
+        {
+            flag = 1;
+        }
+        else
+        {
+            *currx -= 10;
+            printer(*currx, *curry, arr, C, R);
+        }
+    }
+    else if (a[0] == 's')
+    {
+        if (*curry >= R - 10)
+        {
+            flag = 1;
+        }
+        else
+        {
+            *curry += 10;
+            printer(*currx, *curry, arr, C, R);
+        }
+    }
+    else
+    {
+        printf("unrecognized command");
+    }
+
+    if (flag)
+    {
+        printf("Invalid\n");
+    }
+    else
+    {
+        printf("Ok\n");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
         printf("Usage: %s <rows> <columns>\n", argv[0]);
         return 1;
     }
@@ -20,7 +100,8 @@ int main(int argc, char *argv[]) {
     int C = atoi(argv[2]);
 
     int *arr = (int *)calloc(R * C, sizeof(int));
-    if (arr == NULL) {
+    if (arr == NULL)
+    {
         printf("Memory allocation failed\n");
         return 1;
     }
@@ -28,70 +109,25 @@ int main(int argc, char *argv[]) {
     int currx = 0;
     int curry = 0;
 
-    printer(currx, curry, arr, C);
+    printer(currx, curry, arr, C, R);
     char *a = (char *)malloc(200 * sizeof(char));
 
-    int flag = 0;
-    while (1) {
-
+    while (1)
+    {
         scanf(" %s", a);
 
-        if (a[0] == 'w') {
-            if (curry < 10) {
-                flag = 1;
-            } else {
-                curry -= 10;
-                printer(currx, curry, arr, C);
-
-       
-            }
-        } else if (a[0] == 'd') {
-            if (currx > C - 10) {
-                flag = 1;
-            } else {
-
-                currx += 10;
-                printer(currx, curry, arr, C);
-            }
-        } else if (a[0] == 'a') {
-            if (currx < 10) {
-                flag = 1;
-            } else {
-                currx -= 10;
-                printer(currx, curry, arr, C);
-                
-            }
-
-            }
-        } else if (a[0] == 's') {
-            if (curry > R - 10) {
-                flag = 1;
-            } else {
-                curry += 10;
-                printer(currx, curry, arr, C);
-            }
-        } else if (a[0] == 'q') {
+        if (a[0] == 'q')
+        {
             break;
         }
-        else {
-            printf("unrecognized command");
+        else
+        {
+            scroller(a, arr, &currx, &curry, C, R);
         }
-        if (flag) {
-            printf("Invalid\n");
-            flag = 0;
-        } else {
-            printf("Ok\n");
-        }
-
-        free(a);
-        char *a = (char *)malloc(200 * sizeof(char));
     }
 
     free(arr);
+    free(a);
 
     return 0;
 }
-
-
-
-

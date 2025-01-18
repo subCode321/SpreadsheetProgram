b1cf541f78459233291a982950842548656a5719
vanigupta262 <vani21226@gmail.com>
2025-01-18 14:33:32 +0530

    Added display.h

---
+++ display.h
@@ -0,0 +1,7 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+void printer(int currx, int curry, int *arr, int C, int R);
+void scroller(char *a, int *arr, int *currx, int *curry, int C, int R);
+
+#endif 

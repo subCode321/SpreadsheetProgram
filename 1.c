fb2c2f13f25f2272fb8a7067d73221d11cdeec30
vanigupta262 <vani21226@gmail.com>
2025-01-18 15:20:15 +0530

    Added timing of ops in 1.c

--- 1.c
+++ 1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "display.h"
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
@@ -24,20 +25,58 @@ int main(int argc, char *argv[])
     int curry = 0;
 
     printer(currx, curry, arr, C, R);
+
     char *a = (char *)malloc(200 * sizeof(char));
+
+    if (a == NULL)
+    {
+        printf("Memory allocation failed\n");
+        free(arr);
+        return 1;
+    }
 
     while (1)
     {
+        printf("> ");
         scanf(" %s", a);
 
         if (a[0] == 'q')
         {
             break;
         }
+        // else
+        // {
+        //     scroller(a, arr, &currx, &curry, C, R);
+        // }
+        clock_t start = clock(); // Start timing
+
+        int status = 1;
+
+        
+        if (a[0] == 'w' || a[0] == 'a' || a[0] == 's' || a[0] == 'd')
+        {
+            scroller(a, arr, &currx, &curry, C, R); 
+        }
         else
         {
-            scroller(a, arr, &currx, &curry, C, R);
+            status = parser(a, C, R, arr); 
         }
+
+        clock_t end = clock(); 
+        double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+        printer(currx, curry, arr, C, R); 
+
+        
+        if (status)
+        {
+            printf("[%.6f] (ok)\n", time_taken);
+        }
+        else
+        {
+            printf("[%.6f] (error: invalid command)\n", time_taken);
+        }
+        
 
         free(a);
         free(arr);

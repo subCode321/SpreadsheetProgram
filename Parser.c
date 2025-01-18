3a2f0b5e299af86e64134e88bb6114bdc56ae75e
vanigupta262 <vani21226@gmail.com>
2025-01-18 15:50:33 +0530

    Added timing of ops in 1.c

--- Parser.c
+++ Parser.c
@@ -47,7 +47,7 @@ void arth_op(char *a, int C, int R, int pos_equalto, int pos_end)
     }
 }
 
-void func(char *a, int C, int R, int pos_equalto, int pos_end)
+void func(char *a, int C, int R, int pos_equalto, int pos_end, int *arr)
 {
     int first_cell;
 
@@ -72,6 +72,42 @@ void func(char *a, int C, int R, int pos_equalto, int pos_end)
         {
             if (a[pos_equalto + 1] == 'M' && a[pos_equalto + 2] == 'I' && a[pos_equalto + 3] == 'N')
             {
+                char *colon_pos = strchr(a, ':');
+
+                if (colon_pos == NULL)
+                {
+                    printf("Invalid range: Missing ':'\n");
+                    return;
+                }
+
+                int range_start = cell_parser(a, C, R, pos_equalto + 5, colon_pos - a - 1);
+                int range_end = cell_parser(a, C, R, colon_pos - a + 1, pos_end - 1);
+
+                if (range_start == -1 || range_end == -1)
+                {
+                    printf("Invalid range\n");
+                    return;
+                }
+
+                
+                int min_value = arr[range_start];
+                for (int idx = range_start + 1; idx <= range_end; idx++)
+                {
+                    if (arr[idx] < min_value)
+                    {
+                        min_value = arr[idx];
+                    }
+                }
+
+                
+                int dest_cell = cell_parser(a, C, R, 0, pos_equalto - 1);
+                if (dest_cell == -1)
+                {
+                    printf("Invalid destination cell\n");
+                    return;
+                }
+
+                arr[dest_cell] = min_value; 
             }
             else if (a[pos_equalto + 1] == 'M' && a[pos_equalto + 2] == 'A' && a[pos_equalto + 3] == 'X')
             {

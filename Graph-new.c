431711d89e35a37550a6c0d80237e1662f66665a
Soubhagya Mohapatra <34794687+subCode321@users.noreply.github.com>
2025-01-25 22:35:17 +0530

    added new files (#15)

--- Graph-new.c
+++ Graph-new.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include "math.h"
+#include "avl.h"
+
 #define NUM_CELLS 18259722
 
 typedef struct Cell
@@ -11,6 +13,8 @@ typedef struct Cell
     struct Cell *prev;
     int op_type;
     int op_info;
+    struct Node *root;
+
 } Cell;
 
 typedef struct Graph
@@ -32,7 +36,16 @@ Cell *Addcell(int cell, int op_type, int op_info, Cell *prev)
     new_cell->next = NULL;
     new_cell->prev = prev;
     new_cell->op_type = op_type;
-    new_cell->op_info = op_info;  
+    new_cell->op_info = op_info;
+    new_cell->root = createNode(cell);
+
+    if (!new_cell->root)
+    {
+        printf("Cannot allocate memory for AVL tree root for %d\n", cell);
+        free(new_cell);
+        return NULL;
+    }
+
     return new_cell;
 }
 
@@ -67,14 +80,17 @@ Graph *CreateGraph()
 void Addedge(int cell1, int cell2, int op_type, int op_info, Graph *graph)
 {
     Cell *new_cell = Addcell(cell1, op_type, op_info, graph->adjLists_tail[cell2]);
+    Cell *head_cell = Addcell(cell2, op_type, op_info, NULL);
+
     if (!new_cell)
         return;
 
     if (graph->adjLists_head[cell2] == NULL)
     {
         // printf("%d %d \n", cell1, cell2);
-        graph->adjLists_head[cell2] = new_cell;
+        graph->adjLists_head[cell2] = head_cell;
         graph->adjLists_tail[cell2] = new_cell;
+        
     }
     else
     {
@@ -82,6 +98,7 @@ void Addedge(int cell1, int cell2, int op_type, int op_info, Graph *graph)
         graph->adjLists_tail[cell2]->next = new_cell;
         graph->adjLists_tail[cell2] = new_cell;
     }
+    graph->adjLists_head[cell2]->root = insert(graph->adjLists_head[cell2]->root, cell1);
 }
 /*
 Op                    op_type   op_info
@@ -100,15 +117,69 @@ Op                    op_type   op_info
 (STDEV)                  13      |Range|
 (SLEEP)                  14       NULL
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+// A helper function for DFS traversal to perform topological sort
+void DFS(int node, Graph *graph, int *visited, int *topoArray, int *index)
+{
+    visited[node] = 1;
+
+    // Traverse through all adjacent cells of the node
+    Cell *curr = graph->adjLists_head[node];
+    while (curr != NULL)
+    {
+        if (!visited[curr->cell])
+        {
+            DFS(curr->cell, graph, visited, topoArray, index);
+        }
+        curr = curr->next;
+    }
+
+    // After exploring all the adjacent nodes, add the current node to the topological array
+    topoArray[(*index)++] = node;
+}
+
+// Function to perform topological sort
+void toposort(Graph *graph, int *topoArray)
+{
+    int *visited = (int *)malloc(NUM_CELLS * sizeof(int));
+    int index = 0;
+
+    // Initialize all nodes as unvisited
+    for (int i = 0; i < NUM_CELLS; i++)
+    {
+        visited[i] = 0;
+    }
+
+    // Perform DFS for every unvisited node
+    for (int i = 0; i < NUM_CELLS; i++)
+    {
+        if (!visited[i])
+        {
+            DFS(i, graph, visited, topoArray, &index);
+        }
+    }
+
+    // Reverse the topoArray to get the correct topological order
+    for (int i = 0; i < NUM_CELLS / 2; i++)
+    {
+        int temp = topoArray[i];
+        topoArray[i] = topoArray[NUM_CELLS - i - 1];
+        topoArray[NUM_CELLS - i - 1] = temp;
+    }
+
+    free(visited);
+}
 
 void Recalc(int cell, int new_value, Graph *graph, int *cell_values)
 {
     int old_value = cell_values[cell];
     cell_values[cell] = new_value;
 
-   
     Cell *x = graph->adjLists_head[cell];
-    if (x == NULL) {
+    if (x == NULL)
+    {
         // printf("Returning");
         return;
     }
@@ -120,7 +191,8 @@ void Recalc(int cell, int new_value, Graph *graph, int *cell_values)
     {
         int dependent_new_value = cell_values[x->cell]; // Default value
 
-        if (x->op_type ==0){
+        if (x->op_type == 0)
+        {
             dependent_new_value = new_value;
         }
 
@@ -156,14 +228,13 @@ void Recalc(int cell, int new_value, Graph *graph, int *cell_values)
         {
             dependent_new_value = cell_values[x->cell] / old_value * new_value;
         }
-        else if(x->op_type == 8)
+        else if (x->op_type == 8)
         {
             dependent_new_value = cell_values[x->cell] * old_value / new_value;
         }
-        else if(x->op_type == 9)
+        else if (x->op_type == 9)
         {
             dependent_new_value = min(new_value, cell_values[x->cell]);
-
         }
         else if (x->op_type == 10)
         {
@@ -177,9 +248,6 @@ void Recalc(int cell, int new_value, Graph *graph, int *cell_values)
         {
             dependent_new_value = cell_values[x->cell] - old_value + new_value;
         }
-    
-
-            
 
         // Update dependent cell and recursively recalculate
         Recalc(x->cell, dependent_new_value, graph, cell_values);
